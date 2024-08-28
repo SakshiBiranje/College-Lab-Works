67d00c42670fee71e87738c88d5afd7ed1eade19
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-08-28 21:05:16 +0530

    update

--- DBMS_LAB/Create_table.sql
+++ DBMS_LAB/Create_table.sql
@@ -9,3 +9,13 @@ update b_ranch set branch_name ='kolhapur' where branch_id = '1'
 
 delete from b_ranch where branch_id = '3'
 SELECT* FROM b_ranch;
+
+create table D_kte(fc_code Number(10) Primary key , Department_n Varchar(10) Not Null ,CET_P Number(5) Not null ,Phone_n Number (10) );
+insert into D_kte values('14442','CSE','98.12','7242526210');
+insert into D_kte values('14542','CSE-AI','95.12','7242556210');
+insert into D_kte values('14642','ENTC','88.12','9242526210');
+insert into D_kte values('14742','Mechanical','78.12','7242526210');
+desc D_kte
+select * from D_kte ;
+update D_kte set CET_P='78.12' where fc_code = '14742';
+
