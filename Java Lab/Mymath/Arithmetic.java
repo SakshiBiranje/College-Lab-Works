8076f20f4f6fd89ce9f737a0aa136bd71ab13dc5
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-14 13:46:50 +0530

    add

--- Java Lab/Mymath/Arithmetic.java
+++ Java Lab/Mymath/Arithmetic.java
@@ -1,16 +1,20 @@
 package MyMath;
 public class Arithmetic
 {
-     public float add(float a, float b) {
+     public float add(float a, float b) 
+    {
        return a + b;
-     }
-    public float subtract(float a, float b) {
+    }
+    public float subtract(float a, float b) 
+    {
         return a - b;
-     }
-    public float multiply(float a,float b) {
+    }
+    public float multiply(float a,float b) 
+    {
         return a*b;
     }
-    public float divide(float a, float b) {
+    public float divide(float a, float b) 
+    {
         if (b == 0) {
             return 0;
          } else {
