6790f0c5e0cc04e8b1634db8ab3cdeb069fe9f80
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-10-19 21:50:22 +0530

    Update

--- ML-Lab Python/KNN_k=1.py
+++ ML-Lab Python/KNN_k=1.py
@@ -12,6 +12,7 @@ new_point = [(new_x, new_y)]
 
 prediction = knn.predict(new_point)
 
+
 plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
 plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
 plt.show()
