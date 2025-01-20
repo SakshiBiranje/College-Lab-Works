2ec4b97f37018a9b820c99725c077fae0d47abfe
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-20 19:20:36 +0530

    Create Program2.java

---
+++ CDAC_Java/Program2.java
@@ -0,0 +1,11 @@
+
+public class Program2 {
+    public static void main(String[] args) {
+        int side = 10 ;
+        double area = side *side;
+        double  perimeter = 4 *side;
+        System.out.println("Area of square : "+ area);
+        System.out.println("Perimeter of square : "+ perimeter);
+    }
+}
+
