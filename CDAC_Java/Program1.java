5839d562d52a3d91489c4b1f799cf23bc0eae80c
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-20 19:20:34 +0530

    Create Program1.java

---
+++ CDAC_Java/Program1.java
@@ -0,0 +1,11 @@
+public class Program1 {
+    public static void main(String[] args){
+        int r = 5;
+        double pi=Math.PI;
+        double area = pi* r*r;
+        double circumference = 2* pi*r;
+        System.out.println("Radius of circle : "+ r);
+        System.out.println("Enter the area  : "+ area);
+        System.out.println("Enter the circumference  : "+ circumference);
+    }
+}
