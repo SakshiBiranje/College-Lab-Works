a21303e90f672680e266238bfcb3de160d8604c0
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-21 22:21:18 +0530

    Create Triangle.java

---
+++ CDAC_Java/Triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Triangle {
+ private int s1;
+ private int s2;
+ private int s3;
+ public Triangle(){
+
+ }
+ public void GetSide(int s1, int s2 , int s3){
+    Scanner sc = new Scanner(System.in);
+ }
+ public void SetSide(int s1, int s2 , int s3){
+
+ }
+}
