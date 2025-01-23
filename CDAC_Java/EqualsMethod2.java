abaca7b635426fb962263e260cad14e994773cbb
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-01-23 18:55:11 +0530

    Create EqualsMethod2.java

---
+++ CDAC_Java/EqualsMethod2.java
@@ -0,0 +1,49 @@
+class Employee 
+{
+	private String name; 
+	private int empid; 
+	private float salary;
+	public Employee() {
+		// unparameterized constuctor
+	}
+	public Employee(String name, int empid, float salary) {
+		//parameterized constuctor
+		this.name = name;
+		this.empid = empid;
+		this.salary = salary;
+	} 
+
+	//this = emp1 
+	//obj = emp2 
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null)
+			return false;
+		if(this == obj)
+			return true;
+		if(!(obj instanceof Employee))
+		return false;
+		Employee other =(Employee) obj;
+		if(this.empid == other.empid){
+			return true;
+		} 	
+		return false;
+		
+		
+	}
+}
+
+
+public class EqualsMethod2 {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee("Aditya",1,1000.00f); 
+		Employee emp2 = new Employee("Aditya",2,1000.00f); 
+		if(emp1.equals(emp2))
+			System.out.println("Same");
+		else 
+			System.out.println("not same");
+	}
+	
+
+}
