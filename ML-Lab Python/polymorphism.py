81520fa04993ef703f821e9e52eb547381d85949
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-06-19 11:01:11 +0530

    Create polymorphism.py

---
+++ ML-Lab Python/polymorphism.py
@@ -0,0 +1,12 @@
+#polymorphism in function
+
+#method overloading and overriding are the examples of polymorphism
+#opearator overloading
+def add(a=0,b=0,c=0):
+    print(a+b+c)
+    
+    add()
+    add(1)
+    add(1,2)
+    add(1,2,3)
+    
