f64c6a3946c972e6fd6e7977520fdd695fdb5610
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-07-16 13:37:56 +0530

    Create practice1.java

---
+++ CDAC_Java/practice1.java
@@ -0,0 +1,26 @@
+/*data types- 1. linear = arrays, arraylist , stack , queue etc.
+                2. nonlinear = strings, hash table , tree, graph etc
+
+*/
+import java.util.*;
+public class practice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements : ");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        int sum=0;
+        for(int num : arr)
+            {
+                sum += num;
+            }
+            System.out.println("Sum : "+ sum);
+            Arrays.sort(arr);
+            System.out.println("Min value : "+arr[0]);
+            System.out.println("Max value : "+arr[n-1]);
+    }
+}
