0b120919b9f37599eaea714c6808a898d30f5f34
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-09-10 21:49:30 +0530

    Create 22UAI011_Experiment2.js

---
+++ AWD_lab/22UAI011_Experiment2.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.get("/22UAI011_Experiment2", function (req, res) {
+    const date = new Date().toLocaleString();
+    res.send("Hello World <br> Current Timestamp: " + date);
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
