21deea71e65d0b47f92a02aa79e911042bd5ae15
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-10-30 18:26:51 +0530

    Create app.routes.ts

---
+++ AWDL/Angular/Demo1/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import {  Routes } from '@angular/router';
+import {DataBinding} from './data-binding/data-binding'
+export const routes: Routes = [
+    {
+        path:'data-binding', component:DataBinding
+    }
+
+];
