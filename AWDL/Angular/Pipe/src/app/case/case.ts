868b53fe5c7680e3d571db1adfbb615d7272a812
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-10-31 18:28:28 +0530

    Create case.ts

---
+++ AWDL/Angular/Pipe/src/app/case/case.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case',
+  imports: [],
+  templateUrl: './case.html',
+  styleUrl: './case.css'
+})
+export class Case {
+
+}
