809f249174dac4ff0258574b46391cf114489cd2
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2025-11-29 18:24:22 +0530

    Create app.ts

---
+++ AWDL/Angular/app/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Admin } from './admin/admin';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Admin],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {
+  
+}
